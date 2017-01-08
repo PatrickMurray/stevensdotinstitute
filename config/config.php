d2a3001858a6b9fc0f13708a45e182a2a080a93a
Patrick Murray <patrickm129@gmail.com>
2017-01-08 15:58:55 -0500

    test commit

--- config/config.php
+++ config/config.php
@@ -21,12 +21,14 @@ $CONFIGURATION = [
 
 if ($CONFIGURATION['PRODUCTION'])
 {
+	ini_set('log_errors', 1);
 	ini_set('error_reporting', 0);
 	ini_set('display_errors', 0);
 	ini_set('display_startup_errors', 0);
 }
 else
 {
+	ini_set('log_errors', 1);
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
