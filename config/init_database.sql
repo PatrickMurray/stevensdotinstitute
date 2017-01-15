8d91147d9f6af72892af93fe6f775ad270a3e952
Patrick Murray <patrickm129@gmail.com>
2017-01-14 23:05:32 -0500

    correcting sql syntax errors

--- config/init_database.sql
+++ config/init_database.sql
@@ -5,7 +5,7 @@ USE stevensdotinstitute;
 
 
 CREATE TABLE IF NOT EXISTS Boards (
-	`id`                 UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,
+	`id`                 INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	`abbreviation`       VARCHAR(3)       NOT NULL,
 	`title`              VARCHAR(16)      NOT NULL,
 	`description`        VARCHAR(256)     NOT NULL,
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS Boards (
 
 
 CREATE TABLE IF NOT EXISTS Threads (
-	`board_id` UNSIGNED INTEGER NOT NULL,
-	`op_id`    UNSIGNED INTEGER NOT NULL,
+	`board_id` INTEGER UNSIGNED NOT NULL,
+	`op_id`    INTEGER UNSIGNED NOT NULL,
 	`title`    VARCHAR(64),
 
 	PRIMARY KEY (board_id, id),
@@ -31,16 +31,16 @@ CREATE TABLE IF NOT EXISTS Threads (
 
 
 CREATE TABLE IF NOT EXISTS Posts (
-	`board_id`          UNSIGNED INTEGER NOT NULL,
-	`thread_id`         UNSIGNED INTEGER NOT NULL,
-	`id`                UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,
+	`board_id`          INTEGER UNSIGNED NOT NULL,
+	`thread_id`         INTEGER UNSIGNED NOT NULL,
+	`id`                INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 
 	`creation_datetime` DATETIME         NOT NULL DEFAULT NOW(),
 	`ip_address_hash`   BINARY(60)       NOT NULL,
 
 	`name`              VARCHAR(32),
 	`comment`           TEXT,
-	`file_id`           UNSIGNED INTEGER,
+	`file_id`           INTEGER UNSIGNED,
 
 	PRIMARY KEY (board_id, thread_id, id),
 	FOREIGN KEY (board_id)  REFERENCES Boards(id)  ON DELETE CASCADE,
@@ -50,13 +50,13 @@ CREATE TABLE IF NOT EXISTS Posts (
 
 
 CREATE TABLE IF NOT EXISTS Files (
-	`id`                UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,
+	`id`                INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 
 	`creation_datetime` DATETIME         NOT NULL DEFAULT NOW(),
 	`ip_address_hash`   BINARY(60)       NOT NULL,
 	`poster_name`       VARCHAR(32),
 
-	`size`              UNSIGNED INTEGER NOT NULL,
+	`size`              INTEGER UNSIGNED NOT NULL,
 	`hash`              BINARY(32)       NOT NULL,
 	`mime_type`         VARCHAR(255)     NOT NULL,
 	`content`           MEDIUMBLOB       NOT NULL
