1e19f60abfdda7bc8f10c6eaced235eef18cf6c6
Patrick Murray <patrickm129@gmail.com>
2017-01-14 22:54:10 -0500

    removing database initialization from PHP, TODO : README.md server setup tutorial

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@ function db_conn($hostname, $db_name, $username, $password)
 	try
 	{
 		$connection = new PDO(
-			'mysql:host=' . $hostname,
+			'mysql:host=' . $hostname . ';dbname=' . $db_name,
 			$username,
 			$password
 		);
@@ -28,63 +28,6 @@ function db_conn($hostname, $db_name, $username, $password)
 		log_error(__FILE__, __LINE__, 'failed to connect to database: '. $exception->getMessage());
 		throw new RuntimeException('connection failure');
 	}
-
-	$sql = 'CREATE DATABASE IF NOT EXISTS ' . $db_name . ';';
-	$connection->query($sql);
-
-	$sql = 'USE ' . $db_name . ';';
-	$connection->query($sql);
-
-	$sql  = 'CREATE TABLE IF NOT EXISTS Boards (';
-	$sql .= '	`id`                 UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,';
-	$sql .= '	`abbreviation`       VARCHAR(3)       NOT NULL,';
-	$sql .= '	`title`              VARCHAR(16)      NOT NULL,';
-	$sql .= '	`description`        VARCHAR(256)     NOT NULL,';
-	$sql .= '	`creation_datetime`  DATETIME         NOT NULL DEFAULT NOW(),';
-	$sql .= '	`published_status`   BOOLEAN          NOT NULL DEFAULT 0,';
-	$sql .= '	`published_datetime` DATETIME,';
-	$sql .= '	`new_branding`       BOOLEAN          NOT NULL DEFAULT 1,';
-	$sql .= '	PRIMARY KEY (id)';
-	$sql .= ');';
-	$connection->query($sql);
-
-	$sql  = 'CREATE TABLE IF NOT EXISTS Threads (';
-	$sql .= '	`board_id` UNSIGNED INTEGER NOT NULL,';
-	$sql .= '	`op_id`    UNSIGNED INTEGER NOT NULL,';
-	$sql .= '	`title`    VARCHAR(64),';
-	$sql .= '	PRIMARY KEY (board_id, id),';
-	$sql .= '	FOREIGN KEY (board_id) REFERENCES Boards(id) ON DELETE CASCADE,';
-	$sql .= '	FOREIGN KEY (op_id)    REFERENCES Posts(id)  ON DELETE CASCADE';
-	$sql .= ');';
-	$connection->query($sql);
-
-	$sql .= 'CREATE TABLE IF NOT EXISTS Posts (';
-	$sql .= '	`board_id`          UNSIGNED INTEGER NOT NULL,';
-	$sql .= '	`thread_id`         UNSIGNED INTEGER NOT NULL,';
-	$sql .= '	`id`                UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,';
-	$sql .= '	`creation_datetime` DATETIME         NOT NULL DEFAULT NOW(),';
-	$sql .= '	`ip_address_hash`   BINARY(60)       NOT NULL,';
-	$sql .= '	`name`              VARCHAR(32),';
-	$sql .= '	`comment`           TEXT,';
-	$sql .= '	`file_id`           UNSIGNED INTEGER,';
-	$sql .= '	PRIMARY KEY (board_id, thread_id, id),';
-	$sql .= '	FOREIGN KEY (board_id)  REFERENCES Boards(id)  ON DELETE CASCADE,';
-	$sql .= '	FOREIGN KEY (thread_id) REFERENCES Threads(id) ON DELETE CASCADE,';
-	$sql .= '	FOREIGN KEY (file_id)   REFERENCES Files(id)   ON DELETE CASCADE';
-	$sql .= ');';
-	$connection->query($sql);
-
-	$sql .= 'CREATE TABLE IF NOT EXISTS Files (';
-	$sql .= '	`id`                UNSIGNED INTEGER NOT NULL AUTO_INCREMENT,';
-	$sql .= '	`creation_datetime` DATETIME         NOT NULL DEFAULT NOW(),';
-	$sql .= '	`ip_address_hash`   BINARY(60)       NOT NULL,';
-	$sql .= '	`poster_name`       VARCHAR(32),';
-	$sql .= '	`size`              UNSIGNED INTEGER NOT NULL,';
-	$sql .= '	`hash`              BINARY(32)       NOT NULL,';
-	$sql .= '	`mime_type`         VARCHAR(255)     NOT NULL,';
-	$sql .= '	`content`           MEDIUMBLOB       NOT NULL';
-	$sql .= ');';
-	$connection->query($sql);
 
 	return $connection;
 }
