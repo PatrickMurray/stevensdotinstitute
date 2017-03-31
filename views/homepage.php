49a55ac857b4fa899d7e9ddc230a5c58def1d788
Patrick Murray <patrickm129@gmail.com>
2017-03-30 22:28:32 -0400

    adding HTML to the homepage view

--- views/homepage.php
+++ views/homepage.php
@@ -1,5 +1,20 @@
-<h1>Homepage</h1>
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>stevens dot institute</title>
+		<link rel="stylesheet" type="text/css" href="/assets/stylesheets/main.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="logo">
+					stevens <sub>dot</sub> institute
+				</div>
+
+				<nav>
+					<ul>
+<?
 
 
 if (!isset($boards))
@@ -12,7 +27,34 @@ if (!isset($boards))
 	error_internal_error();
 }
 
-var_dump($boards);
+
+foreach ($boards as $board)
+{
+	print("\t\t\t\t\t\t<li><a href=\"" . $board["abbreviation"] . "\">/" . $board["abbreviation"] . "/</a></li>")
+}
 
 
 ?>
+				</nav>
+			</div>
+		</header>
+		<div class="container main">
+			<h1>What is stevens.institute?</h1>
+
+			<p>stevens.institute is an imageboard designed for the stevens community. It's a
+			place for open, anonymous collaboration and discussion. Within the various boards
+			you'll find topics ranging from campus life and events to media and technology
+			news. The epemeral nature of the platform means that topics stick around only as
+			long as they're relevant: once a thread is pruned for inactivity, it's gone for
+			good. Best of all, there's no accounts to manage, you can just jump right in and
+			post.</p>
+
+			<p>Please take a look at our <a href="/rules">Rules</a> page for information
+			about what is and is not acceptible on the platform.</p>
+
+			<p>stevens.institute is not affiliated with or endorsed by Stevens Institute of
+			Technology. The messages posted within do not necessarily reflect the views of
+			the school or the developers.</p>
+		</div>
+	</body>
+</html>
